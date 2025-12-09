3135f27bee49d759225aee84d3e8ae8ca5e20b76
habib23me <habib23me@gmail.com>
2025-12-09 23:15:15 +0400

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Habib Mohammed on 09/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
